c8360444cb0111dd2593e63c5793c68a4231096d
galaxydestroyer276 <galaxydestroyer276@protonmail.com>
2026-05-18 10:14:15 +0530

    feat: add fish abbreviations for git commands

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,5 +16,25 @@ if status is-interactive
 
     abbr --add q exit
     abbr --add w z # zoxide
-    abbr --add e nvim 
+    abbr --add e nvim
+
+    abbr --add g git
+    abbr --add ga git add
+    abbr --add gaa git add --all
+    abbr --add gb git branch
+    abbr --add --set-cursor gcm git commit -m \"%\"
+    abbr --add --set-cursor gcam git commit -a -m \"%\"
+    abbr --add gco git checkout
+    abbr --add gd git diff
+    abbr --add gds git diff --staged
+    abbr --add gl git log
+    abbr --add gpl git pull
+    abbr --add gr git reset
+    abbr --add gr! git reset --hard
+    abbr --add grh git reset HEAD
+    abbr --add grh! git reset HEAD --hard
+    abbr --add gs git status
+    abbr --add gss git status -s
+    abbr --add gsb git status -sb
+    abbr --add gt git tag
 end
