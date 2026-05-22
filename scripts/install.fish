b99bbccd4b672e24567ab0f4db7513ae451d3e04
galaxydestroyer276 <galaxydestroyer276@protonmail.com>
2026-05-22 16:42:21 +0530

    feat: ensure auto-installation of Bibata Cursors

--- scripts/install.fish
+++ scripts/install.fish
@@ -21,7 +21,7 @@ function backup_old_dotfiles
     mkdir -p $DOTFILES_BACKUP_DIR
     for item in $(get_dotfiles_item_list)
         if test -d $item -a -e $item
-            cp -vrf $item $DOTFILES_BACKUP_DIR/
+            cp -rf $item $DOTFILES_BACKUP_DIR/
         end
     end
 end
@@ -38,13 +38,52 @@ end
 function cleanup_old_dotfiles
     for item in $(get_dotfiles_item_list)
         if test -d $item -a -e $item
-            rm -vrf $item
+            rm -rf $item
         end
     end
 end
 
 function install_new_dotfiles
     cp -rf $DOTFILES_REPO_PATH/.config/* $HOME/.config/
+end
+
+function ensure_bibata_cursors
+    set -l icons_dir $HOME/.local/share/icons
+    set -l variants Bibata-Original-Ice Bibata-Modern-Ice
+    set -l bibata_repo \
+        "https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7"
+
+    mkdir -p $icons_dir
+
+    for variant in $variants
+        if test -d $icons_dir/$variant
+            continue
+        end
+
+        echo "Installing $variant..."
+
+        set -l temp_dir (mktemp -d)
+        set -l fname "$variant.tar.xz"
+        set -l archive "$temp_dir/$fname"
+        set -l url "$bibata_repo/$fname"
+
+        if not curl -L $url -o $archive 2>/dev/null
+            echo "ERROR: Failed to download $fname"
+            rm -rf $temp_dir
+            return 1
+        end
+
+        if not tar -xf $archive -C $icons_dir 2>/dev/null
+            echo "ERROR: Failed to extract $fname"
+            rm -rf $temp_dir
+            return 1
+        end
+
+        rm -rf $temp_dir
+    end
+
+    echo "✔ Bibata cursors installed"
+    return 0
 end
 
 # --- Main Execution Flow ---
@@ -60,3 +99,5 @@ end
 if test ! -e $HOME/.local/bin/ruff
     $HOME/.local/bin/uv tool install ruff@latest
 end
+
+ensure_bibata_cursors
